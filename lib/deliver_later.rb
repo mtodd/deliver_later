dcb29dc510589035781ab9c9ab0e580859041d7c
Matt Todd <chiology@gmail.com>
2009-01-14 11:47:28 -0500

    Added version information and documentation for the delivery queueing process.

--- lib/deliver_later.rb
+++ lib/deliver_later.rb
@@ -1,16 +1,37 @@
 module DeliverLater
   
+  VERSION = [0, 2, 1]
+  
+  class << self
+    
+    def version
+      VERSION.join('.')
+    end
+    
+  end
+  
   def method_missing(delivery_method, *args)
-    if delivery_method.to_s =~ /\Aperform_delivery_(\w+)_later\Z/ and
-       delivery_method = $1
-       # respond_to?("perform_delivery_#{delivery_method}".to_sym)
-      send("perform_delivery_later", delivery_method, *args)
+    if delivery_method.to_s =~ /\Aperform_delivery_(\w+)_later\Z/
+      send("perform_delivery_later", *args)
     else
       super
     end
   end
   
-  def perform_delivery_later(delivery_method, mail)
+  # Enqueue the mail to be delivered in the 2nd phase.
+  # 
+  # This method is called when the delivery method is appended with
+  # <tt>_later</tt>, such as:
+  # 
+  #   config.actionmailer.delivery_method = :smtp_later
+  # 
+  # Since +perform_delivery_smtp_later+ does not exist, it gets caught and sent
+  # to this method to be queued.
+  # 
+  # Any <tt>_later</tt> delivery method will be queued up unless one is
+  # specifically added.
+  # 
+  def perform_delivery_later(mail)
     QueuedMailing.enqueue(mail)
   end
   
